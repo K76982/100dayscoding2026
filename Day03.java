fd329f494dc5a3a70ba0238cf4b2e861613020f1
MuhammadAydil1 <dil803811@gmail.com>
2026-09-04 02:16:30 +0800

    Create Day03.java

---
+++ Day03.java
@@ -0,0 +1,19 @@
+public class EscapeSequence {
+    public static void main(String[] args) {
+        //untuk menampilkan tanda kutip//
+        System.out.println("Halo saya \"Asep\"");
+
+        //untuk menampilkan tanda blackslash (\)
+        System.out.println("C:\\Users\\Java");
+
+        //untuk menambahkan jarak antar kalimat
+        System.out.println("Nama:\tMuh Asep");
+        System.out.println("Umur:\t21 tahun");
+
+        //untuk membuat baris selanjutnya
+        System.out.println("\nFakultas:");
+        System.out.println("Teknik");
+        System.out.println("\nProdi:");
+        System.out.println("Informatika");
+    }
+}
