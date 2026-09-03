3b5f55f0f489b4ebc9cfeb7727869b73b6d7d09d
MuhammadAydil1 <dil803811@gmail.com>
2026-09-03 21:00:28 +0800

    Create Day02.java

---
+++ Day02.java
@@ -0,0 +1,21 @@
+public class Biodata {
+    public static void main(String[] args) {
+        
+        // Menggunakan println
+        System.out.println("=== BIODATA DIRI ===");
+        System.out.println("Nama      : Muh Aydil");
+        System.out.println("NIM       : D0224028");
+        
+        // Menggunakan print
+        System.out.print("Jurusan   : ");
+        System.out.print("Teknik Informatika\n");
+        
+        System.out.print("Fakultas  : ");
+        System.out.print("Ilmu Komputer\n");
+        
+        // Menggunakan printf
+        System.out.printf("Tempat Lahir : %s%n", "Wonomulyo");
+        System.out.printf("Umur         : %d tahun%n", 21);
+        System.out.printf("IPK          : %.2f%n", 3.75);
+    }
+}
