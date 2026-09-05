973508e1c21cf09cb6debb9305bc8104f755b1b1
MuhammadAydil1 <dil803811@gmail.com>
2026-09-05 21:34:13 +0800

    Add Day04.java with basic data types and output

---
+++ Day04.java
@@ -0,0 +1,13 @@
+public class day04 {
+    public static void main(String[] args) {
+        byte bb = 49;
+        short tb = 161;
+        int populasiKebakaran = 1500000;
+        long hargaBBM = 500000000000l;
+
+        System.out.println("Berat Badan : "  + bb + "kg");
+        System.out.println("Tinggi Badan : " + tb + "cm");
+        System.out.println("Populasi kebakaran : " + populasiKebakaran + "juta");
+        System.out.println("Harga : " + "Rp" +hargaBBM);
+    }
+}
