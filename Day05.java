9eaf89cd86cd2d5d15d5f34c9f8f4fae6dc3ad5b
MuhammadAydil1 <dil803811@gmail.com>
2026-09-06 23:45:54 +0800

    Add Day05 class with main method for output

---
+++ Day05.java
@@ -0,0 +1,9 @@
+public class Day05 {
+    public static void main(String[] args) {
+        float SB = 36.5f;
+        double PB = 2.800000000000;
+
+        System.out.println("Suhu Badan : " + SB + " derajat");
+        System.out.println("Pembayaran UKT : "  + PB +"juta");
+    }
+}
