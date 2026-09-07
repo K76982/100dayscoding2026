36f543d863f01160d5be950b310d64e22d2e484d
MuhammadAydil1 <dil803811@gmail.com>
2026-09-07 22:34:29 +0800

    Add Day06 class with main method and outputs

---
+++ Day06.java
@@ -0,0 +1,12 @@
+public class Day06 {
+    public static void main(String[] args) {
+
+                char Fa = 'F';
+                boolean Lu = true;
+
+                System.out.println("Nilai: " + Fa);
+                System.out.println("Status Lulus: " + Lu);
+
+
+    }
+}
