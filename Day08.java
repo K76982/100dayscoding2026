9b969e9c35bf8509412e1aee4abe93927ffe97ad
MuhammadAydil1 <dil803811@gmail.com>
2026-09-09 23:27:20 +0800

    Add Day08 class to display age

---
+++ Day08.java
@@ -0,0 +1,10 @@
+public class Day08 {
+    public static void main(String[] args) {
+        int umur;
+        umur = 21;
+        System.out.println("Umur: " + umur);
+
+        umur = 22;
+        System.out.println("Umur yang diperbarui : " + umur);
+    }
+}
