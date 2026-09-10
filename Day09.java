5bc1759fb336c812a929821dcec142612cd41a16
MuhammadAydil1 <dil803811@gmail.com>
2026-09-10 22:12:53 +0800

    Add Day09 class with main method and output

---
+++ Day09.java
@@ -0,0 +1,12 @@
+public class  Day09{
+    public static void main(String[] args) {
+
+        final String Nama = "Muhammad Aydil";
+        final double IPK = 3.14;
+
+        System.out.println("Nama saya: " + Nama);
+        System.out.println("Nilai IPK: " + IPK);
+
+
+    }
+}
