a5102bae9cf76a413dd7c61e416471f2eaf560b3
MuhammadAydil1 <dil803811@gmail.com>
2026-09-11 21:49:55 +0800

    Add Day10 class to display min and max values

---
+++ Day10.java
@@ -0,0 +1,15 @@
+public class Day10 {
+    public static void main(String[] args) {
+        // Integer (int)
+        System.out.println("Min Integer : " + Integer.MIN_VALUE); 
+        System.out.println("Max Integer : " + Integer.MAX_VALUE); 
+
+        // Byte
+        System.out.println("Min Byte    : " + Byte.MIN_VALUE);    
+        System.out.println("Max Byte    : " + Byte.MAX_VALUE);    
+
+        // Double
+        System.out.println("Min Double  : " + Double.MIN_VALUE);  
+        System.out.println("Max Double  : " + Double.MAX_VALUE);  
+    }
+}
