b61d2c136e63f94b84a35d8e846e7a6b3aebefe6
MuhammadAydil1 <dil803811@gmail.com>
2026-09-16 21:55:01 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,11 @@
+public class Day15 {
+	public static void main(String[] args) {
+	
+        int a = 35;
+        int b = 5;
+
+        System.out.println("perkalian =  " + a * b);
+        System.out.println("pembagian =  " + a / b);
+   
+	}
+}
