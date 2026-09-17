9be58c6ba35971b522ebf6b323ab18a52df28949
MuhammadAydil1 <dil803811@gmail.com>
2026-09-17 22:21:02 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,10 @@
+public class Day16 {
+    public static void main(String[] args) {
+        int permen = 17;
+        int orang = 5;
+
+        int sisa = permen % orang;
+
+        System.out.println("Sisa permen: " + sisa);
+    }
+}
