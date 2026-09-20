d6d479fc89fa6d27fd167209bcedfa23f7e89c7e
MuhammadAydil1 <dil803811@gmail.com>
2026-09-20 23:07:43 +0800

    Add Day19.java for type casting example

---
+++ Day19.java
@@ -0,0 +1,10 @@
+public class Day19 {
+    public static void main(String[] args) {
+
+        double angka = 100.75;
+        int hasil = (int) angka;
+
+        System.out.println("Nilai double : " + angka);
+        System.out.println("Nilai int    : " + hasil);
+    }
+}
