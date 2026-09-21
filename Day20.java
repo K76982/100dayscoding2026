ffe2172b821e1b56e6c17f772833949cbfa3c28a
MuhammadAydil1 <dil803811@gmail.com>
2026-09-21 23:12:04 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,8 @@
+public class Day20 {
+    public static void main(String[] args) {
+        double nilai = 85.5;
+
+        String nilaiString = String.valueOf(nilai);
+        System.out.println("double menjadi String: " + nilaiString);
+    }
+}
