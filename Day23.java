29ac8dbc9dd71ae945873767649992be84e0911d
MuhammadAydil1 <dil803811@gmail.com>
2026-09-24 23:31:52 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,11 @@
+public class Day23 {
+    public static void main(String[] args) {
+
+        int panjangSisi = 8;
+
+        int hasilLuas = panjangSisi * panjangSisi;
+
+        System.out.println("Panjang sisi = " + panjangSisi);
+        System.out.println("Luas persegi = " + hasilLuas);
+    }
+}
